f1e9dfbe56d6d872a3545db11adbf79efda447f5
Sankar <Sankar.TN@xome.com>
2025-06-19 23:02:35 +0530

    Base Set UP

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Link from "next/link";
+import { Container, Navbar } from "react-bootstrap";
+
+export default function NavBar(){
+    return(
+        <>
+            <Navbar sticky="top" className="portfolio-navbar">
+                <Container className="navbar-container">
+                    <div className="inner-container">
+                        <div className="name-container">
+                            <span>Sankar</span>
+                        </div>
+                        <div id="pages-nav-container">
+                            <div className="page-url">
+                                <Link href="/Home">Home</Link>
+                            </div>
+                            <div className="page-url">
+                                <Link href="/About">About</Link>
+                            </div>
+                            <div className="page-url">
+                                <Link href="/Portfolio">Portfolio</Link>
+                            </div>
+                            <div className="page-url">
+                                <Link href="/Contact">Contact</Link>
+                            </div>
+                        </div>
+                        <div className="resume-btn-cotainer">
+                            <span>View My Resume</span>
+                        </div>
+                    </div>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
